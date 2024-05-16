f734f4fde9876cd09c7c6475adc95c098f4c0f74
N-Siam <nizamuddinsiam2001@gmail.com>
2024-05-17 01:58:41 +0600

    install react router dom and use NavLink in navbar and implement the conditional css properties

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
-import Nav from "../components/Nav";
 
 const Home = () => {
     return (
         <div>
-            <div className="h-16">
-            <Nav></Nav>
-            </div>
             <h1>Homepage</h1>
         </div>
     );
